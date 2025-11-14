59e8ebfb442a892e45fa103dc560b20826ec3b87
David_CB <73202205+DavidCrzBlt@users.noreply.github.com>
2025-11-14 13:29:39 -0600

    Update models_registry.py

--- ml_utils_stacking/stacking_factory/models_registry.py
+++ ml_utils_stacking/stacking_factory/models_registry.py
@@ -1,2 +1,17 @@
-MODEL_CLASSES = {}
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.svm import SVR
+from lightgbm import LGBMRegressor
+import xgboost as xgb
+
+MODEL_CLASSES = {
+    "DecisionTree": DecisionTreeRegressor,
+    "RandomForest": RandomForestRegressor,
+    "HGB Regressor": HistGradientBoostingRegressor,
+    "XGBoost": xgb.XGBRegressor,
+    "SVR": SVR,
+    "LightGBM": LGBMRegressor,
+}
+
 MODEL_MAPPER = {}
